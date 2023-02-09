48b3b47d82bf5471df053d1f628e940b7f45c9d7
dongsung.pae <paeds915@gmail.com>
2023-02-09 19:05:54 +0900

    http 모듈로 html 열기

--- server.js
+++ server.js
@@ -1,18 +1,41 @@
 const http = require('http');
+const fs = require('fs').promises;
 
 
-const server = http.createServer((req,res)=>{
+const server1 = http.createServer(async (req,res)=>{
+    try{
+        if(req.method === 'GET') {
+            if(req.url === '/user'){
+                const data = await fs.readFile('./NAVER.html');
+                res.writeHead(200, {'Content-Type':"text/html; charset=utf-8"});
+                res.end(data);
+            }
+        }        
+    } catch (err) {
+        console.log(err);
+        res.writeHead(500, {'Content-Type':"text/html; charset=utf-8"});
+        res.end(err.message);
+    }
+});
+
+const server2 = http.createServer((req,res)=>{
     res.writeHead(200, {'Content-Type':"text/html; charset=utf-8"});
     res.write('<h1>hello node!</h1>');
-    res.end('<p> hello server DSPlay!</p>');
+    res.end('<p> hello serve!</p>');
 });
-
-server.listen(8080);
-
-server.on('listening', () => {
+ 
+server1.listen(8080);
+server1.on('listening', () => {
     console.log('8080번 포트에서 서버 대기중입니다.')
 });
-
-server.on('error',(error)=>{
+server1.on('error',(error)=>{
     console.error(error);
-})
+});
+
+server2.listen(8081);
+server2.on('listening', () => {
+    console.log('8081번 포트에서 서버 대기중입니다.')
+});
+server2.on('error',(error)=>{
+    console.error(error);
+});
